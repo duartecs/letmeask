16cf4901343a06cd0aa2b2bddf9fc127129e5db6
ismagaia <matheusduartec96@gmail.com>
2021-06-22 10:42:39 -0300

    Conectando o app com o Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Button text="Botao 1" />
+      <Button />
+      <Button />
+      <Button />
     </div>
   );
 }
