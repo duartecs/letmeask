59167035d6fb81a7e7e19b73b669a1aec9b54cf0
ismagaia <matheusduartec96@gmail.com>
2021-06-23 09:17:18 -0300

    Pagina 'NewHome' criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 import { Home } from "./Pages/Home";
+import { NewHome } from "./Pages/NewHome";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewHome} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
